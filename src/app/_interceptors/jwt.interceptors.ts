140fbe36e31ee4bac178076fcb4ceecf4e1da227
Indireshvar-proj <v.indireshvaremail@gmail.com>
2025-07-19 03:50:01 +0530

    new change

--- src/app/_interceptors/jwt.interceptors.ts
+++ src/app/_interceptors/jwt.interceptors.ts
@@ -7,14 +7,21 @@ export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('jwtToken');
 
+    // Clone the request to add Authorization header if token exists
+    let clonedRequest = request;
     if (token) {
-      request = request.clone({
+      clonedRequest = clonedRequest.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
         }
       });
     }
 
-    return next.handle(request);
+    // Always add withCredentials: true to send cookies
+    clonedRequest = clonedRequest.clone({
+      withCredentials: true
+    });
+
+    return next.handle(clonedRequest);
   }
 }
